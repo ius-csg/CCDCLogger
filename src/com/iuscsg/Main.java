b7e119de7a3b8f4d7364a8affbc5a753ea4eb245
TheOtherUnknown <aci747@engineer.com>
2018-12-14 16:53:07 -0500

    Make channel a class var, add "Other event" option

--- src/com/iuscsg/Main.java
+++ src/com/iuscsg/Main.java
@@ -7,19 +7,20 @@ public class Main {
     private static String machine;
     private static IRCBot bot = new IRCBot();
     private static Scanner scan = new Scanner(System.in);
+    private static String chan = "#events";
 
     public static void main(String[] args) {
         bot.setVerbose(true); // Remove me later
-        System.out.println("Who are you? Enter a name: ");
+        System.out.print("Who are you? Enter a name: ");
         name = scan.next();
-        System.out.println("What machine are you working on? This can be changed later");
+        System.out.print("What machine are you working on? This can be changed later");
         machine = scan.next();
-        System.out.println("Enter an IRC server address without port number: ");
+        System.out.print("Enter an IRC server address without port number: ");
         String server = scan.next();
-        System.out.println("Enter the port number, or press enter for the default (6667): ");
+        System.out.print("Enter the port number, or press enter for the default (6667): ");
         String port = scan.nextLine();
-        System.out.println("Connecting...");
         try {
+            System.out.println("Connecting...");
             if (port.isEmpty()) {
                 bot.connect(server);
             } else {
@@ -29,15 +30,16 @@ public class Main {
             System.out.println("Unable to connect to IRC server! Check your server settings, and make sure your nick is not in use.");
             System.exit(1);
         }
-        bot.joinChannel("#events");
+        bot.joinChannel(chan);
         System.out.println("Connected to IRC using nick " + bot.getNick());
         System.out.println("################################################################################################");
         while(true) {
             System.out.println("0. Log local user password change");
             System.out.println("1. Log service password change");
-            System.out.println("2. Change machines (Current: " + machine + ")");
-            System.out.println("3. Change name (Current: " + name + ")");
-            System.out.println("4. Change IRC bot nick (Current: " + bot.getNick() + ")");
+            System.out.println("2. Other event");
+            System.out.println("3. Change machines (Current: " + machine + ")");
+            System.out.println("4. Change name (Current: " + name + ")");
+            System.out.println("5. Change IRC bot nick (Current: " + bot.getNick() + ")");
             System.out.println("-1. Quit the program");
             int option = scan.nextInt();
 
@@ -45,9 +47,21 @@ public class Main {
             {
                 case 0: passwordLocal(); break;
                 case 1: passwordService(); break;
-                case 2: System.out.println("Enter the new machine name: "); machine = scan.next(); break;
-                case 3: System.out.println("Enter a new name: "); name = scan.next();  break;
-                case 4: System.out.println("Enter a new nick: "); bot.changeNick(scan.next()); break;
+                case 2:
+                    otherEvent();
+                    break;
+                case 3:
+                    System.out.print("Enter the new machine name: ");
+                    machine = scan.next();
+                    break;
+                case 4:
+                    System.out.print("Enter a new name: ");
+                    name = scan.next();
+                    break;
+                case 5:
+                    System.out.print("Enter a new nick: ");
+                    bot.changeNick(scan.next());
+                    break;
                 default: bot.quitServer("Exited logger"); System.exit(0);
             }
         }
@@ -55,19 +69,24 @@ public class Main {
     }
 
     public static void passwordLocal() {
-        System.out.println("Enter the username: ");
+        System.out.print("Enter the username: ");
         String username = scan.next();
-        bot.sendMessage("#events", "Local user password for " + username + " changed on " + machine + " by " + name);
+        bot.sendMessage(chan, "Local user password for " + username + " changed on " + machine + " by " + name);
 
     }
 
     public static void passwordService() {
-        System.out.println("Enter a service name: ");
+        System.out.print("Enter a service name: ");
         String service = scan.next();
-        System.out.println("Enter a username: ");
+        System.out.print("Enter a username: ");
         String username = scan.next();
-        bot.sendMessage("#events", "Service " + service + " user password for " + username + " changed on " + machine + " by " + name);
+        bot.sendMessage(chan, "Service " + service + " user password for " + username + " changed on " + machine + " by " + name);
 
+    }
+
+    public static void otherEvent() {
+        System.out.print("What did you do?: ");
+        bot.sendMessage(chan, "Custom event: " + scan.next() + " changed on " + machine + " by " + name);
     }
 
 
